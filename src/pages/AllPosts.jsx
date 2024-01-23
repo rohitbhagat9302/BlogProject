0ff39181eedef131951213e72e42b0a09399295e
Rohit <rohitbhagat9302@gmail.com>
2024-01-23 16:15:24 +0530

    first commit

---
+++ src/pages/AllPosts.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import {Container, PostCard} from '../components'
+import appwriteService from '../appwrite/config'
+
+function AllPosts() {
+    const [post , setPost] = useState([])
+
+    appwriteService.getPosts([]).then((post) => {
+        if(post){
+            setPost(post.documents)
+        }
+    })
+  return (
+    <div className=' w-full py-8'>
+      <Container>
+         <div className='flex flex-wrap'>
+                {post.map((post) => (
+                    <div key={post.$id} className='p-2 w-1/4'>
+                        <PostCard {...post} />
+                    </div>
+                ))}
+         </div>
+      </Container>
+    </div>
+  );
+}
+
+export default AllPosts;
